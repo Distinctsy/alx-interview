e1a7c5ffce5d0181338cbd9ac0883fd8c84e4f01
Distinctsy <oluwaseyiomi@gmail.com>
2023-10-19 13:34:37 +0100

    python

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,55 @@
 #!/usr/bin/python3
 
 import sys
+import signal
 
+def compute_metrics(lines):
+    total_size = 0
+    status_codes = {}
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
+    for line in lines:
+        parts = line.split()
+        if len(parts) != 9:
+            continue
 
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+        ip, _, _, status, size = parts[0], parts[8], parts[10], parts[12], parts[13]
+        if status.isnumeric():
+            status = int(status)
+        else:
+            continue
 
+        if status in [200, 301, 400, 401, 403, 404, 405, 500]:
+            if status in status_codes:
+                status_codes[status] += 1
+            else:
+                status_codes[status] = 1
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+        if size.isnumeric():
+            total_size += int(size)
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+    return total_size, status_codes
 
-        if len(parsed_line) > 2:
-            counter += 1
+def print_metrics(total_size, status_codes):
+    print(f"Total file size: {total_size}")
+    for status in sorted(status_codes):
+        print(f"{status}: {status_codes[status]}")
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
+lines = []
+count = 0
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+def signal_handler(sig, frame):
+    global count
+    count += 1
+    print_metrics(*compute_metrics(lines))
+    if count == 10:
+        sys.exit(0)
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
+signal.signal(signal.SIGINT, signal_handler)
 
-finally:
-    print_msg(dict_sc, total_file_size)
+for line in sys.stdin:
+    lines.append(line)
+    if len(lines) == 10:
+        print_metrics(*compute_metrics(lines))
+        lines = []
+
+print_metrics(*compute_metrics(lines))
